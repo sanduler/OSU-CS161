4ee2dc449ea07c49574c9404a50f1a6a756423f1
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-19 16:12:49 -0700

    finished the implementation header for the Team class

--- Assignment 6/Team.h
+++ Assignment 6/Team.h
@@ -5,11 +5,40 @@
 *******************************************************************************/
 #ifndef ASSIGNMENT_6_TEAM_H
 #define ASSIGNMENT_6_TEAM_H
-
+#include "Player.h"
 
 class Team {
+private:
+    Player PointGuard,
+           ShootingGuard,
+           SmallForward,
+           PowerForward,
+           Center;
+public:
 
+    //constructors
+    Team();
+
+    //overloaded constructors
+    Team(Player, Player, Player, Player, Player);
+
+    //setters
+    void setPointGuard(int);
+    void setShootingGuard(int);
+    void setSmallForward(int);
+    void setPowerForward(int);
+    void setCenter(int);
+
+    //getters
+    int getPointGuard();
+    int getShootingGuard();
+    int getSmallForward();
+    int getPowerForward();
+    int getCenter();
+
+    //total
+    int totalPoints();
 };
 
 
-#endif //ASSIGNMENT_6_TEAM_H
+#endif
