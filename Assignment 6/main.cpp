04e879574813c350c99c3576b83f4ad43e1743c5
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-19 16:47:44 -0700

    added test players for Player class

--- Assignment 6/main.cpp
+++ Assignment 6/main.cpp
@@ -8,6 +8,13 @@
 #include "Team.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to the Sport Arena" << std::endl;
+    Player p1("Charlotte", 24, 10, 7);
+    Player p2("Emily", 21, 13, 9);
+    Player p3("Anne", 20, 10, 8);
+    Player p4("Jane", 19, 10, 10);
+    Player p5("Mary", 18, 11, 8);
+    p5.setRebounds(12);
+
     return 0;
 }
